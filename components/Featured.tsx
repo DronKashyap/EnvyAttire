0ae287860932c6d0e28353e746c198ef9ea2c2f0
DronKashyap <kashyapdron@gmail.com>
2024-09-26 19:03:38 +0530

    modified homepage;

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -1,14 +1,47 @@
-function Featured() {
-  return (
-    <div>
-      <div>
-        Featured
-      </div>
-      <div>
-        product images
-      </div>
-    </div>
-  )
-}
+// "use client";
+// import { useState, useEffect } from "react";
+// import { fetchProductsByCategory } from "../lib/fetchdata";
+// import ProductCard from "./ui/Productcard";
 
-export default Featured
+// interface FeaturedProps {
+//   brandid:number;
+// }
+
+
+// const Featured: React.FC<FeaturedProps> = ({ brandid }) => {
+
+//   const [data, setData] = useState<any[]>([]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const productsData = await fetchProductsByCategory(brandid); // Directly use the fetched data
+//         setData(productsData); // Set the state with the fetched data
+//       } catch (error) {
+//         console.error("Error fetching products:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [brandid]);
+
+//     console.log(data);
+//   return (
+//     <div>
+//       <div className="flex justify-between">
+//         {data.slice(0, 4).map((product, index) => ( // Limit to first 4 products
+//           <ProductCard 
+//             key={index}
+//             name={product.name}
+//             imgurl={product.imageUrl}
+//             price={product.price}
+//           />
+//         ))}
+//       </div>
+//       <div className="flex justify-end">
+//        <a href={`/brands/${brandid}`} className="font-bold text-red-500 underline text-m" > Browse more products </a> 
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Featured
--- components/ui/Productcard.tsx
+++ components/ui/Productcard.tsx
@@ -1,53 +1,27 @@
-// import Image from 'next/image';
-
-// interface ProductCardProps {
-//   price: number;
-//   imgurl: string;
-//   name: string;
-// }
-
-// const Productcard: React.FC<ProductCardProps> = ({ name, price, imgurl }) => {
-//   return (
-//     <div className='flex flex-col relative h-[100px] border-2 p-2 w-[300px]'>
-//       <div className='relative h-full w-full'>
-//         <Image 
-//           src={imgurl} 
-//           alt={name} 
-//           layout='fill' 
-//           objectFit='contain' 
-//           className='rounded-t-lg'
-//         />
-//       </div>
-//       <div className='flex justify-between p-2'> 
-//         <h1 className='text-lg font-semibold'>{name}</h1>
-//         <h1 className='text-lg font-semibold'>${price}</h1>
-//       </div>
-//       <button className='border-red-500 border-2 p-1 rounded-2xl text-red-500 bg-white hover:bg-red-500 hover:text-white'>
-//         Add to cart
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default Productcard;
-
 import Image from 'next/image';
 
-function ProductCard() {
+interface ProductCardProps {
+  price: number;
+  imgurl: string;
+  name: string;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ name, price, imgurl }) => {
+  const absoluteImgUrl = imgurl.startsWith('http') ? imgurl : `https://${imgurl}`;
   return (
     <div className='flex flex-col  relative h-[500px] p-2 w-[300px]'>
       <div className='relative  rounded-3xl h-full w-full overflow-hidden'>
         <Image 
-          src="/sampleproductpic.jpg" 
+          src={absoluteImgUrl}
           alt="product" 
-          layout='fill' 
-          objectFit='contain' 
+          fill
+          className="object-cover rounded-md" 
           
         />
       </div>
       <div className='flex justify-between p-2'> 
-        <h1 className='text-lg font-semibold'>Product Name</h1>
-        <h1 className='text-lg font-semibold'>$100</h1>
+        <h1 className='text-m font-semibold'>{name}</h1>
+        <h1 className='text-lg font-semibold'>${price}</h1>
       </div>
       <button className='border-red-500 border-2 p-1 rounded-2xl text-red-500 bg-white hover:bg-red-500 hover:text-white'>
         Add to cart
